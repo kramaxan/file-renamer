9a3fbe560811826fc5875ea37762eb730c059ee1
kramaxan <sendtomaxkravets@gmail.com>
2024-10-15 22:50:04 +0300

    added support for macOS and Linux

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # File Renamer
 
-A simple file renamer. So far only for Windows OS and only for media files: .png/.jpg pictures and .mp4 videos.
+A simple file renamer. So far working on Windows, macOS and Linux, and only for media files: .png/.jpg images and .mp4 videos.
 
 ## Usage hint
 
-At the stage of specifying the path to the folder with files, please specify the path to the directory where the folder itself is located without specifying its name. Its name will be requested in the next step. By the way, it will serve as a common name for all files inside it.
+At the stage of specifying the path to the folder with files, please specify the path to the directory where the folder itself is located without specifying its name and additional slashes at the end. Its name will be requested in the next step. By the way, it will serve as a common name for all files inside it.
 
 ## Installation
 
@@ -16,4 +16,4 @@ Clone the project and run:
 
 To start the project, run:
 
-    $ npm run start
+    $ npm start
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 import inquirer from 'inquirer';
+import { isNotJunk } from 'junk';
 import { readdirSync, statSync, renameSync } from 'fs';
+
+const CURRENT_OS = process.platform;
+
+if (!['darwin', 'linux', 'win32'].includes(CURRENT_OS)) {
+    console.error('Your OS is currently not supported.');
+    process.exit(1);
+}
+
+let systemSeparator;
+
+switch (CURRENT_OS) {
+    case 'win32':
+        systemSeparator = '\\';
+        break;
+    case 'darwin':
+    case 'linux':
+        systemSeparator = '/';
+        break;
+    default:
+        throw 'Your OS is currently not supported.';
+}
 
 console.log('Welcome to File Renamer!\nPlease:');
 
@@ -25,20 +47,20 @@ const questions = [
 inquirer
     .prompt(questions)
     .then((answers) => {
-        let pathToFiles = `${answers.path}\\${answers.name}`;
-        let files = readdirSync(pathToFiles);
+        let pathToFiles = `${answers.path}${systemSeparator}${answers.name}`;
+        let files = readdirSync(pathToFiles).filter(isNotJunk);
         let sortedFiles = files.sort((a, b) => {
-            let c = statSync(`${pathToFiles}\\${a}`).mtime;
-            let d = statSync(`${pathToFiles}\\${b}`).mtime;
+            let c = statSync(`${pathToFiles}${systemSeparator}${a}`).mtime;
+            let d = statSync(`${pathToFiles}${systemSeparator}${b}`).mtime;
             return c - d;
         });
 
         console.log('Start renaming...');
 
         sortedFiles.forEach((file, index) => {
-            let oldFilePath = `${pathToFiles}\\${file}`;
+            let oldFilePath = `${pathToFiles}${systemSeparator}${file}`;
             let newFile = `${answers.name} (${++index})${answers.extension}`;
-            let newFilePath = `${pathToFiles}\\${newFile}`;
+            let newFilePath = `${pathToFiles}${systemSeparator}${newFile}`;
 
             renameSync(oldFilePath, newFilePath, (err) => console.log(err));
 
