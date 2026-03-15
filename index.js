dcea6dc6ad46ddcac57f44e3145dc80d5088076c
kramaxan <sendtomaxkravets@gmail.com>
2026-03-15 18:08:16 +0200

    bugfix and refactoring

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 import inquirer from 'inquirer';
 import { isNotJunk } from 'junk';
-import { readdirSync, statSync, renameSync } from 'fs';
+import { sep as separator } from 'path';
+import { existsSync, readdirSync, statSync, renameSync } from 'fs';
 
-const CURRENT_OS = process.platform;
-
-if (!['darwin', 'linux', 'win32'].includes(CURRENT_OS)) {
-    console.error('Your OS is currently not supported.');
+if (!['darwin', 'linux', 'win32'].includes(process.platform)) {
+    console.warn('Your OS is currently not supported.');
     process.exit(1);
-}
-
-let systemSeparator;
-
-switch (CURRENT_OS) {
-    case 'win32':
-        systemSeparator = '\\';
-        break;
-    case 'darwin':
-    case 'linux':
-        systemSeparator = '/';
-        break;
-    default:
-        console.error('Your OS is currently not supported.');
-        process.exit(1);
 }
 
 console.log('Welcome to File Renamer!\nPlease:');
@@ -31,6 +15,15 @@ const questions = [
         type: 'input',
         name: 'path',
         message: 'Provide path to files:',
+        validate: (input) => {
+            if (!input.trim()) {
+                return 'Path cannot be empty.';
+            }
+            if (!existsSync(input)) {
+                return 'The provided path does not exist.';
+            }
+            return true;
+        },
     },
 ];
 
@@ -39,30 +32,71 @@ inquirer
     .then((answers) => {
         console.log('Preparing...');
 
-        const PATH_TO_FILES = answers.path;
-        const FOLDER_NAME = PATH_TO_FILES.slice(PATH_TO_FILES.lastIndexOf(systemSeparator) + 1);
+        const { path } = answers;
 
-        let files = readdirSync(PATH_TO_FILES).filter(isNotJunk);
-        let sortedFiles = files.sort((a, b) => {
-            let c = statSync(`${PATH_TO_FILES}${systemSeparator}${a}`).mtime;
-            let d = statSync(`${PATH_TO_FILES}${systemSeparator}${b}`).mtime;
-            return c - d;
-        });
+        const sortedFiles = getSortedFiles(path);
 
+        console.log(`Found ${sortedFiles.length} files in the directory.`);
         console.log('Start renaming...');
 
-        sortedFiles.forEach((oldFileName, index) => {
-            let oldFilePath = `${PATH_TO_FILES}${systemSeparator}${oldFileName}`;
-            let fileExtension = oldFileName.slice(oldFileName.lastIndexOf('.'));
-            let newFileName = `${FOLDER_NAME} (${++index})${fileExtension}`;
-            let newFilePath = `${PATH_TO_FILES}${systemSeparator}${newFileName}`;
+        firstRename(sortedFiles, path);
 
-            renameSync(oldFilePath, newFilePath, (err) => console.log(err));
+        console.log('Finishing...');
 
-            console.log(`${oldFileName}  ->  ${newFileName}`);
-        });
+        const renamedFiles = getSortedFiles(path);
+
+        secondRename(renamedFiles, path);
+
+        console.log('Renaming completed successfully!');
     })
-    .then(
-        () => console.log('Renaming completed successfully!'),
-        (err) => console.log(err)
-    );
+    .catch((error) => {
+        if (error instanceof Error && error.name === 'ExitPromptError') {
+            console.log('See you next time!');
+        } else {
+            throw error;
+        }
+    });
+
+function getSortedFiles(path) {
+    const files = readdirSync(path).filter(isNotJunk);
+
+    if (files.length === 0) {
+        console.warn('No valid files found in the directory.');
+        process.exit(1);
+    }
+
+    const sortedFiles = files.sort((a, b) => {
+        let c = statSync(`${path}${separator}${a}`).mtime.getTime();
+        let d = statSync(`${path}${separator}${b}`).mtime.getTime();
+        return c - d;
+    });
+
+    return sortedFiles;
+}
+
+function firstRename(files, path) {
+    const folderName = path.slice(path.lastIndexOf(separator) + 1);
+
+    files.forEach((fileName, index) => {
+        const filePath = `${path}${separator}${fileName}`;
+        const fileExtension = fileName.slice(fileName.lastIndexOf('.'));
+        const newFileName = `_TEMP_${folderName} (${++index})${fileExtension}`;
+        const newFilePath = `${path}${separator}${newFileName}`;
+
+        renameSync(filePath, newFilePath);
+
+        console.log(`${fileName}  ->  ${newFileName.slice(6)}`);
+    });
+}
+
+function secondRename(files, path) {
+    files.forEach((fileName) => {
+        if (fileName.startsWith('_TEMP_')) {
+            const newFileName = fileName.slice(6);
+            const filePath = `${path}${separator}${fileName}`;
+            const newFilePath = `${path}${separator}${newFileName}`;
+
+            renameSync(filePath, newFilePath);
+        }
+    });
+}
